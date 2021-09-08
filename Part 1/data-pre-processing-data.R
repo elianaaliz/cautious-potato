09222cb311c22c81731cc7cbbaa1a192b2f79d0d
Eliana Aliz <elianaaliz16@gmail.com>
2021-09-08 18:18:30 +0200

    First Steps
    Pre-processing data with R

---
+++ Part 1/data-pre-processing-data.R
@@ -0,0 +1,11 @@
+#Plantilla para el pre-procesado de datos
+#Importar el dataset
+
+dataset = read.csv('Data.csv')
+
+#TRATAMIENTO DE LOS VALORES NA
+dataset$Age = ifelse(is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),dataset$Salary)
+
+
